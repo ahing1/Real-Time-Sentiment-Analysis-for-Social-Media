cfad839e1eb802d7f8a7e01fa081fa137efe9089
ahing1 <ahing1@stevens.edu>
2025-02-13 20:43:39 -0500

    updated backend middleware

--- backend/api_server.py
+++ backend/api_server.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (change to specific domain if needed)
+    allow_origins=["https://sentiment-dashboard.vercel.app"],  # Allow all origins (change to specific domain if needed)
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
