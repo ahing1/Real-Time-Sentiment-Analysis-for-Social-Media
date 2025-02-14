8dd06c610e3b433e96633ef5eae79542486ae5da
ahing1 <ahing1@stevens.edu>
2025-02-13 20:53:11 -0500

    updated port for backend

--- backend/api_server.py
+++ backend/api_server.py
@@ -79,4 +79,5 @@ def get_sentiment_stats():
     return {"stats": stats}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
